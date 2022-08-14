a0155042f7a6902c1b0c9ca2b77826abb6e9c2b0
Lukinhas123Ito <lucastoshihiroito28@gmail.com>
2022-08-14 20:07:40 -0300

    test number 1

---
+++ src/components/first.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function First(){
+
+    return(     
+      <h1>Bananinha frit 1 2 3</h1>
+  );
+}
+
+export default First
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+import React from 'react'
+import reactDOM from 'react-dom'
+
+import First from './components/first'
+
+reactDOM.render(
+    <First></First>,
+    document.getElementById('root')
+
+)
